d249eca0f9d8e7fcf7dc6cf81779f2e801bd2c43
uma-clicksco <uma.pinninti@clicksco.com>
2019-08-06 12:38:46 +0530

    Add redis changes

--- start.py
+++ start.py
@@ -1,11 +1,16 @@
+"""
+Prototype for Real time Stats view
+"""
+
 #import eventlet
 #eventlet.monkey_patch()
+import time
 from threading import Thread
 from pymongo import MongoClient
 from flask import Flask, render_template
 from bson.json_util import dumps
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
-import time
+from flask_socketio import SocketIO, emit, join_room
+import redis
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -17,57 +22,105 @@ socketio = SocketIO(app, async_mode='threading')
 @app.route('/')
 @app.route('/home')
 def index():
+    """
+    Home page
+    :return: Template for home page
+    """
     return render_template('index.html')
 
 
 @app.route('/login')
 def login():
+    """
+    Login page view
+    :return:
+    """
     return render_template('login.html')
 
 
 @app.route('/register')
 def register():
+    """
+    Register page view
+    :return:
+    """
     return render_template('register.html')
 
 
 @app.route('/forgot-password')
 def forget_password():
+    """
+    Forgot password page
+    :return:
+    """
     return render_template('forgot-password.html')
 
 
 @app.route('/dashboard')
 def dashboard():
+    """
+    Dashboard
+    :return:
+    """
     return render_template('dashboard.html')
 
 
 @socketio.on('connect', namespace='/hello')
 def sys_connect():
-    print ('client connected')
+    """
+    Socket connection
+    :return:
+    """
+    print('client connected')
 
 
 @socketio.on('join', namespace='/hello')
 def join(message):
+    """
+    Joining socket room
+    :param message:
+    :return:
+    """
     join_room(message['room'])
-    print ('client joined room %s' % message['room'])
+    print('client joined room %s' % message['room'])
 
 
 @socketio.on('queues_latest', namespace='/hello')
 def latest_queues():
+    """
+    Fetching latest data
+    :return:
+    """
     print('client requested latest queues data')
-    client = MongoClient();
-    db = client.company
-    employeeslist = db.employees
+    client = MongoClient()
+    db_client = client.company
+    employeeslist = db_client.employees
     data = dumps(employeeslist.find())
     emit('queues_data_push', data)
 
 
 def emit_queues():
-    data = {"name": "FM", "spend": time.time(), "wins": time.time()}
+    """
+    Send latest data to client
+    :return:
+    """
+    redis_client = redis.StrictRedis(host="rtb-cache.r1kpgb.ng.0001.usw2.cache.amazonaws.com")
+    wins = redis_client.get("win_70848460555_2019-08-06")
+    spend = redis_client.get("budget_70848460555_2019-08-06")
+    data = {"name": "FM", "spend": spend, "wins": wins}
     socketio.emit('queues_data_push', data, namespace='/hello', room='queues')
 
 
 def start_scheduler():
+    """
+    scheduler
+    :return:
+    """
     def scheduler_loop():
+        """
+        Scheduler loop
+        :return:
+        """
         while True:
             time.sleep(1)
             emit_queues()
